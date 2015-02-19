223c7a5a167d5429d9620d74d6992135eed84dc4
Alex Hughes <User@Lions-Mac.local>
2015-02-19 20:04:59 +0000

    Initial Commit

---
+++ ResearchMonster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ResearchMonster
+//
+//  Created by Alex Hughes on 19/02/2015.
+//  Copyright (c) 2015 SolutionBlender. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
